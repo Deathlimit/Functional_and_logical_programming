715d1494d363df2c5cd7d7899dfaa25d39cceb42
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2025-04-11 22:49:09 +0300

    (lab 2.13 (real)) added functions which moving elements

--- OperationsWithLists/Program.fs
+++ OperationsWithLists/Program.fs
@@ -22,7 +22,53 @@ let isGlobalMaxUsingList (arr: int list) index =
     |> List.item index   
     |> (=) (List.max arr)
 
-let list = [3; 5; 2]
-let index = 1
-System.Console.WriteLine(isGlobalMax list index) 
-System.Console.WriteLine(isGlobalMaxUsingList list index)
+let moveBeforeMinToEndUsingList (arr: int list) =
+    match arr with
+    | [] -> []
+    | _ ->
+        let minVal = List.min arr
+        let minIndex = List.findIndex (fun x -> x = minVal) arr
+        let beforeMin, fromMin = List.splitAt minIndex arr
+        fromMin @ beforeMin
+
+let moveBeforeMinToEnd arr =
+    let rec findMin currentMin = function
+        | [] -> currentMin
+        | head::tail -> findMin (min head currentMin) tail
+
+    let rec findMinIndex minVal index = function
+        | [] -> -1
+        | head::tail -> if head = minVal then index else findMinIndex minVal (index + 1) tail
+
+    let reverseList list =
+        let rec loop acc = function
+            | [] -> acc
+            | head::tail -> loop (head::acc) tail
+        loop [] list
+
+    let append list1 list2 =
+        let reversedList1 = reverseList list1
+        let rec loop acc = function
+            | [] -> acc
+            | head::tail -> loop (head::acc) tail
+        loop list2 reversedList1
+
+    let rec splitAt index acc = function
+        | [] -> (reverseList acc, [])
+        | head::tail as lst ->
+            if index = 0 then (reverseList acc, lst)
+            else splitAt (index - 1) (head::acc) tail
+
+    match arr with
+    | [] -> []
+    | _ ->
+        let minVal = findMin 1000 arr
+        let minIndex = findMinIndex minVal 0 arr
+        if minIndex <= 0 then arr 
+        else
+            let beforeMin, fromMin = splitAt minIndex [] arr
+            append fromMin beforeMin
+
+let list = [5; 3; 1; 2; 4]
+System.Console.WriteLine(moveBeforeMinToEndUsingList list) 
+System.Console.WriteLine(moveBeforeMinToEnd list)
