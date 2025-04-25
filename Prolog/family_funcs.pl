5958867049da302f5d9d09b8ce675bf2518b51b6
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2025-04-25 20:53:05 +0300

    (lab 4 task 1.3) added more family functions

--- Prolog/family_funcs.pl
+++ Prolog/family_funcs.pl
@@ -23,3 +23,52 @@ wife(X) :-
     wife(Y, X),
     print(Y),
     !.
+
+%X является дедушкой Y
+grand_pa(X, Y) :-
+    man(X),              
+    parent(X, Z),          
+    parent(Z, Y),
+    !.          
+
+%Дедушки X
+grand_pas(X) :-
+    grand_pa(Y, X),        
+    print(Y),
+    nl,
+    fail.   
+
+
+%X является бабушкой Y
+grandma(X, Y) :-
+    woman(X),          
+    parent(X, Rod),
+    parent(Rod, Y), вер
+    man(Y).
+
+%X и Y бабушка и внук и наоборот
+grand_ma_and_son(X, Y) :-
+    (grandma(X, Y) ; grandma(Y, X)),
+    X \= Y.   
+
+
+%общий родитель есть?
+sestrabrat(X, Y) :-
+    parent(Z, X),
+    parent(Z, Y),
+    X \= Y.
+
+%X является племянником Y
+plemyannik(X, Y) :-
+    man(X),           
+    parent(Rod, X),    
+    sestrabrat(Y, Rod),   
+    Y \= Rod.          
+
+%Племянники X
+plemyanniks(X) :-
+    plemyannik(Y, X),         
+    print(Y),
+    nl,
+    fail. 
+                      
