21b9a5076cad67e6575c9936f7514132b69d2600
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2025-05-17 01:54:49 +0300

    (Individual Task 1) fixed syntax

--- Individual/133.fs
+++ Individual/133.fs
@@ -2,28 +2,23 @@
 
 let N = 100000
 
-let resheto =
-    let comp = Array.zeroCreate N
-    comp[0] <- true
-    comp[1] <- true
-    
-    let rec markMultiples i j =
-        if j < N then
-            comp[j] <- true
-            markMultiples i (j + i)
-    
-    let rec markPrimes i =
-        let maxI = int (sqrt (float N))
-        if i <= maxI then
-            if not comp[i] then
-                markMultiples i (i * i) 
-            markPrimes (i + 1)
-    
-    markPrimes 2
-    
-    comp
+let primesDoN n =
+    let rec filterMult p xs acc =
+        match xs with
+        | [] -> List.rev acc
+        | i :: rest ->
+            if i % p = 0 then filterMult p rest acc
+            else filterMult p rest (i :: acc)
 
-let comp = resheto 
+    let rec resheto xs acc =
+        match xs with
+        | [] -> List.rev acc
+        | p :: rest ->
+            if p * p > n then (acc @ xs)
+            else resheto (filterMult p rest []) (p :: acc)
+
+    resheto [2 .. n] []
+
 
 let findPeriod p =
     let rec loop j count =
@@ -31,25 +26,31 @@ let findPeriod p =
         else loop ((j * 10) % p) (count + 1)
     loop (10 % p) 1
 
-let delPeriod i =
-    let rec divideBySmth n Smth =
-        if n % Smth = 0 then 
-            divideBySmth (n / Smth) Smth 
-        else 
-            n
+
+let delPeriod period =
+    let rec divideByFactor n del =
+        if n % del = 0 then divideByFactor (n / del) del
+        else n
     
-    let without2 = divideBySmth i 2      
-    divideBySmth without2 5              
+    let without2 = divideByFactor period 2
+    divideByFactor without2 5
+
 
 let sum =
-    seq { 7 .. N-1 }
-    |> Seq.filter (fun p -> not comp[p])
+    primesDoN (N - 1)
+    |> Seq.filter (fun p -> p >= 7)
     |> Seq.map (fun p ->
         let period = findPeriod p
         let new_period = delPeriod period
-        Console.WriteLine($"prost = {p} | period = {period} | delPeriod = {new_period}")
+        printfn "prost = %d | period = %d | delPeriod = %d" p period new_period
         if new_period > 1 then Some p else None)
     |> Seq.choose id
     |> Seq.fold (+) 10
 
-Console.WriteLine($"Сумма = {sum}")
+printfn "Сумма = %d" sum
+
+
+
+let s a b = a + b
+"123" |> s 
+
